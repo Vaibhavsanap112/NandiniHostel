cab1912556b1f6153a8225c70959cfe085a3dbde
sudarshan sangle <darshsangle9456@gmail.com>
2025-04-06 13:32:24 +0530

    Added new component for the homepage Co-authored-by: Vaibhav Sanap <sanapvaibhv8767@gmail.com>

--- Nandini/src/Pages/AboutUs.jsx
+++ Nandini/src/Pages/AboutUs.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 function AboutUs() {
   return (
     <div>
-      
+      <h2>We are offer 1bhk as well 1bhk with warm water facility</h2>
+      <h3>1000|- with other benifits</h3>
     </div>
   )
 }
---
+++ Nandini/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+      <h2>Contact with me: shekar</h2>
+      <h2>Sponcers-Gande tiffin town</h2>
+    </div>
+  )
+}
+
+export default Footer
