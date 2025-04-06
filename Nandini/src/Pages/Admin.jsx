877366effc3559b8b465b295c45f882c5c37d5d2
sudarshan sangle <darshsangle9456@gmail.com>
2025-04-06 13:37:03 +0530

    Added new component for the homepage  Co-authored-by: Vaibhav Sanap <sanapvaibhv8767@gmail.com>

---
+++ Nandini/src/Pages/Admin.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Admin() {
+  return (
+    <div>
+        <h2>User-shekar</h2>
+        <h2>Password-123456</h2>
+      
+    </div>
+  )
+}
+
+export default Admin
---
+++ Nandini/src/Pages/Features.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Features() {
+  return (
+    <div>
+      <h2>Warm water</h2>
+       <h2>free wifi</h2>
+       <h2>Filter cold water</h2>
+    </div>
+  )
+}
+
+export default Features
